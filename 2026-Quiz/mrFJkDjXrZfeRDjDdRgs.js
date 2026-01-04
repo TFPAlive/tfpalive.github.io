962aec87cd20ea81c488116c1eeb6ddbbb98e314
Hong Anh Quan <Anh.Quan.220703@gmail.com>
2026-01-04 23:12:30 +0900

    Refactor code structure for improved readability and maintainability

--- 2026-Quiz/mrFJkDjXrZfeRDjDdRgs.js
+++ 2026-Quiz/mrFJkDjXrZfeRDjDdRgs.js
@@ -1,6 +1,6 @@
 // Download a file when the page loads
 window.addEventListener('load', () => {
-    const fileUrl = 'https://tfpalive.github.io/2026-Quiz/xassiqjwbearjwhqdbywhntyiia.jpg'; // Replace with your file URL
+    const fileUrl = 'https://tfpalive.github.io/2026-Quiz/xassiqjwbearjwhqdbywhntyiia.png'; // Replace with your file URL
     
     const link = document.createElement('a');
     link.href = fileUrl;
