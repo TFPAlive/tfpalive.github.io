46599172445b1e36ca8a9e2f361c086c2474f0c7
Hong Anh Quan <Anh.Quan.220703@gmail.com>
2026-04-04 14:18:28 +0900

    2026-04-04

--- MaimaiXD/test.js
+++
@@ -1,7 +0,0 @@
-// Inject otohime script into the page
-const otohimeScript = document.createElement("script");
-otohimeScript.src = "https://otohi.me/go.js";
-
-document.body.appendChild(otohimeScript);
-
-document.body.setAttribute("data-otohime-token", "6ac0bcdec6fe4cddbabd0acdbbc76346");
